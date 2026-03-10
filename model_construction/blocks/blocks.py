18d934ac13207f938880dbb45b1774079f76d1bb
Bearson678 <sean2002tan@gmail.com>
2026-03-10 17:04:05 +0800

    created the directory structure for the project

---
+++ model_construction/blocks/blocks.py
@@ -0,0 +1,10 @@
+"""
+This module will construct the blocks needed for the model's construction
+
+It should have the following methods:
+
+    1. __init__ : define the parameters, layers and relevant functions used in the block
+    
+    2. forward: define how foward propgation through the block is done 
+
+"""
---
+++ model_construction/model.py
@@ -0,0 +1,16 @@
+"""
+
+This module will construct our deep learning model of choice using blocks of layers
+
+The model class should contain the following methods:
+
+    1.  __init__ : define the parameters, layers and relevant functions used in the architecture
+    
+    2. forward : define how foward propgation through the layers is done 
+    
+    3. train : Update the parameters using an optimizer of choice and follow iterations of forward and backward propagation
+    
+    4. Saver and Loader : Save and load a state of the model (parameters)
+    
+
+"""
